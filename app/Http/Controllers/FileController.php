512b0677bd7792aef6f31b406e01e129493e308d
PicheyUong <puong1@paragoniu.edu.kh>
2024-01-12 21:39:36 +0700

    view change

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -11,9 +11,9 @@ class FileController extends Controller
 {
     public function upload(Request $request)
     {
-        $request->validate([
-            'file' => 'required|mimes:jpg,png,jpeg|max:2048',
-        ]);
+        // $request->validate([
+        //     'file' => 'required|mimes:jpg,png,jpeg|max:2048',
+        // ]);
 
         $file = $request->file('file');
 
@@ -32,35 +32,4 @@ class FileController extends Controller
         return view('view', ['person' => $posts]);
     }
     
-    // public function getImage(Request $request){
-    //   $path = DB::table("person")->find($request->id);
-    //   $photoPath = $path->picture;
-    //   if(Storage::disk('block')->exists($photoPath)){
-    //     $fileContent = Storage::disk('block')->get($photoPath);
-    //     $contentType = pathinfo($photoPath, PATHINFO_EXTENSION);
-    //     return response($fileContent)->header('Content-Type', $contentType);
-    //   }
-    // }
-
-    
-        // if($request->input('type') == '1'){
-        //   $path = $file->store('laravel', 'block');
-        //   DB::table('person')->insert([
-        //       'firstname'=>$request->input('firstname'),
-        //       'lastname'=>$request->input('lastname'),
-        //       'type'=>$request->input('type'),
-        //       'picture'=>$path
-        //   ]);
-          
-        //   return response()->json(['path' => $path]);
-        // } else if($request->input('type') == '0') {
-        //   $path = $file->store('laravel', 'spaces'); // 'uploads' is the folder in your Space
-        //   DB::table('person')->insert([
-        //       'firstname'=>$request->input('firstname'),
-        //       'lastname'=>$request->input('lastname'),
-        //       'type'=>$request->input('type'),
-        //       'picture'=>$path
-        //   ]);
-        //   return response()->json(['path' => $path]);
-        // }
 }
